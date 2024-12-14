f9d58b2261a1e33fbcd0424fa6541d4badadf8c2
Ubuntu2404LTS <brianschmitt001@yahoo.com>
2024-12-14 10:57:16 -0800

    updated TFretBoard

--- fretest.pas
+++ fretest.pas
@@ -15,7 +15,9 @@ begin
   writeln ( 'Inches to MM  : ' + FloatToStrF(inchTomm(1), ffFixed,  2, 2));
 
   writeln ('Frets:  ' + FloatToStr(frets.numfrets));
-  
-  // free (frets);
+  writeln (' Single Fret SPacing for 1st Fret  in mm: ' + FloatToStr(frets.singleFret(1)));
+  writeln ( '       in Inches = ' + FloatToStr(mmToInch(frets.singleFret(1))));
+  frets.metric := false;
+  writeln ( ' Internal Conversion in Inches = ' + FloatToStr(frets.singleFret(1)));
 
 end.
